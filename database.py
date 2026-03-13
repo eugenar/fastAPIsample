01b3c5a9e79d62e9bad1818ab7e35df7a57b6abc
eugenar <eugenar@hotmail.com>
2026-03-12 17:06:43 -0700

    docs

--- database.py
+++ database.py
@@ -16,5 +16,6 @@ engine = create_async_engine(db_url, connect_args=connect_args)
 
 
 async def create_db_and_tables():
+    """Creates the database and tables (if they don't exist) based on the defined SQLModel models."""
     async with engine.begin() as conn:
         await conn.run_sync(SQLModel.metadata.create_all)
